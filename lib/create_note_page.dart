9e710ab75c06de024d3a81ec252569395907ac2f
enzo <enzo.danjour@gmail.com>
2021-05-20 19:01:14 -0300

    Recive params with navigation

--- lib/create_note_page.dart
+++ lib/create_note_page.dart
@@ -8,13 +8,27 @@ class CreateNotePage extends StatefulWidget {
 
 class _CreateNotePageState extends State<CreateNotePage> {
   String description = "";
+  var textController = TextEditingController();
+
+  //init state
+  @override
+  void initState() {
+    // inicializa o contexto posteriormente a ser chamado
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
+      //Verifica se os parâmetros passados via homepage estão
+      //sendo recebidos além de definir o tipo dele
+      if (ModalRoute.of(context)!.settings.arguments != null) {
+        // receber dados via navegação
+        description = ModalRoute.of(context)!.settings.arguments as String;
+        //Controller tem o valor do parâmetro recebido
+        textController.text =  description;
+      }
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    //Verifica se os parâmetros passados via homepage estão sendo recebidos
-    if (ModalRoute.of(context)!.settings.arguments != null) {
-      description = ModalRoute.of(context)!.settings.arguments as String;
-    }
     return Scaffold(
       appBar: AppBar(
         title: Text("Create note page"),
@@ -27,6 +41,7 @@ class _CreateNotePageState extends State<CreateNotePage> {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             TextField(
+              controller: textController,
               maxLines: null,
               cursorColor: Colors.amber,
               onChanged: (value) {
@@ -39,7 +54,8 @@ class _CreateNotePageState extends State<CreateNotePage> {
                   onPressed: () {
                     Navigator.pop(context, description);
                   },
-                  child: Text('Salvar'))
+                  child: Text('Salvar')
+                  )
           ],
         ),
       ),
