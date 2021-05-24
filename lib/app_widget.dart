ff8071d3c838830b35c86b7a4e66ce35940f01f4
enzo <enzo.danjour@gmail.com>
2021-05-24 12:04:33 -0300

    Change some imports

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:notes/create_note_page.dart';
-import 'package:notes/home_page.dart';
+import 'package:notes/core/themes.dart';
+import 'package:notes/pages/notes_customize/create_note_page.dart';
+import 'package:notes/pages/home/home_page.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Notes",
+      theme:ThemesApp.light,
       home: HomePage(),
       initialRoute: "/home",
       routes: {
